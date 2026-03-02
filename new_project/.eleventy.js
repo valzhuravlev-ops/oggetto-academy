87ad206a2ee7233696d3b8597efd1bfbe780cbf7
Valery Zhuravlev <valzhuravlev@oggettoweb.com>
2026-03-02 20:23:40 +0700

    пофиксили Бандлинг CSS и JS из компонентов 11ty

--- new_project/.eleventy.js
+++ new_project/.eleventy.js
@@ -2,7 +2,7 @@ const bundlePlugin = require("@11ty/eleventy-plugin-bundle");
 
 module.exports = function (eleventyConfig) {
   // Бандлинг CSS и JS из компонентов
-  eleventyConfig.addPlugin(bundlePlugin);
+  eleventyConfig.addPlugin(bundlePlugin.default);
 
   // Копирование статики без обработки
   eleventyConfig.addPassthroughCopy("src/assets");
