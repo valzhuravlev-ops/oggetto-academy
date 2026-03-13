d85026ca1bce95b3f9ac5e1c0c220e245faaaa67
Valery Zhuravlev <valzhuravlev@oggettoweb.com>
2026-03-13 15:57:54 +0700

    перенесли backend, frontend, qa страницы форм

---
+++ new_project/src/components/application-form-frontend/application-form-frontend.js
@@ -0,0 +1,8 @@
+t_onFuncLoad("t396_initialScale", function () {
+  t396_initialScale("447992341");
+});
+t_onReady(function () {
+  t_onFuncLoad("t396_init", function () {
+    t396_init("447992341");
+  });
+});
